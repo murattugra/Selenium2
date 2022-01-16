b5783e8ea6d00509448a2aa5757adf47b9be5e79
Murat Arslan <mrterkmglsv@gmail.com>
2022-01-16 23:51:54 +0100

    ek

--- src/test/java/ErsterTag/ElementFinden.java
+++ src/test/java/ErsterTag/ElementFinden.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.sql.Driver;
+
 public class ElementFinden {
 
 
@@ -19,12 +21,24 @@ public class ElementFinden {
         driver.manage().window().maximize();
         WebElement nameElement= driver.findElement(By.id("userName"));
         nameElement.click();
-        nameElement.sendKeys("ipek");
+        nameElement.sendKeys("Murat");
+
+        WebElement email = driver.findElement(new By.ByCssSelector(".mr-sm-2[id='userEmail']"));
+        email.click();
+        email.sendKeys("mrtekmasa@gmail.com");
+
+        WebElement currentAdress = driver.findElement( new By.ByCssSelector(".form-control[placeholder='Current Address']"));
+        currentAdress.click();
+        currentAdress.sendKeys("Dneme");
+
+        WebElement PermanenetAdress = driver.findElement(By.id("permanentAddress"));
+
+        PermanenetAdress.click();
+        PermanenetAdress.sendKeys("SonDenem dfasdfsd");
 
 
-
-
-
+        WebElement SubmitButton = driver.findElement(new By.ByCssSelector("button.btn"));
+        SubmitButton.click();
 
 
     }
