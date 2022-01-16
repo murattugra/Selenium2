39dd7b660c76943a8be04f8d15b473bcbb004915
Murat Arslan <mrterkmglsv@gmail.com>
2022-01-16 14:16:57 +0100

    repead

---
+++ src/test/java/ErsterTag/Wiederholung.java
@@ -0,0 +1,28 @@
+package ErsterTag;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Wiederholung {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","src/driver/chromedriver.exe");
+        //ilk once path ve driver ismini giriyorum
+
+        WebDriver driver=new ChromeDriver();
+        // WebDriver objesi olusturuyorum
+        //Bununla driver otomasyonunu kullanmak icin driverim var
+        driver.get("https://linkein.com");
+        //bununla girilen siteye git diyorum
+        Thread.sleep(5000);
+        // gidilen sitete ne kadar bekleyecegini bliliyorum
+        driver.close();
+        // driveri kapatiyorum
+
+    }
+
+
+
+}
